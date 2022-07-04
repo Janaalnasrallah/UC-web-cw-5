ecd538087ffd312bbdbe726893754803a3b1e3e1
Janaalnasrallah <107787730+Janaalnasrallah@users.noreply.github.com>
2022-07-04 16:50:00 +0300

    cw4

---
+++ script.js
@@ -0,0 +1,16 @@
+function BMI(length,width){
+    let result =(length * length)/width;
+    return result ;
+}
+console.log(result); 
+
+function status(BMI){
+    if(BMI >= 25 )
+        console.log("so weight is so high");
+        BMI.style
+    else if (BMI < 25 && BMI >= 18.5)
+        console.log("your weight is normal");
+    else 
+        console.log("your weight is low");
+
+}
